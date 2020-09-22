5b1f2da5736ede73f7554076beb7c7fcc0880785
Carlos Eduardo Díaz Valbuena <cedv90@gmail.com>
2020-09-22 16:55:38 -0500

    Se termina la clase weather

--- functions/src/model/valueobjects/weather.go
+++ functions/src/model/valueobjects/weather.go
@@ -10,7 +10,7 @@ type Weather struct {
 	Betasoide        *Star
 	Vulcano          *Star
 	Ferengi          *Star
-	Perimeter        int
+	Perimeter        float64
 	WeatherCondition WeatherCondition
 	Day              int
 
@@ -25,30 +25,114 @@ func NewWeather(sun Star, day int) *Weather {
 	return w
 }
 
+//Método para inicializar los planetas con su respectiva configuración.
 func (w Weather) initializeStars() {
+	/**
+	 * Se instancian los planetas con:
+	 * Nombre
+	 * Distancia al sol
+	 * Grados
+	 * Velocidad (grados por día)
+	 * Movimiento con respecto a las manecillas del reloj
+	 */
 	w.Betasoide = NewStar("Betasoide", 2000, 0, 3, true)
 	w.Vulcano = NewStar("Vulcano", 1000, 0, 5, false)
 	w.Ferengi = NewStar("Ferengi", 500, 0, 1, true)
 }
 
+//Método para asignar las posiciones correspondientes de los planetas dependiendo del número de día.
 func (w Weather) setPositionByDayNumber() {
+	/**
+	 * Llamado al método que realiza el cálculo de la posición del planeta con respecto al número de día.
+	 */
 	w.Betasoide.SetPositionByDayNumber(w.Day)
 	w.Vulcano.SetPositionByDayNumber(w.Day)
 	w.Ferengi.SetPositionByDayNumber(w.Day)
 
+	/**
+	 * Llamado al cálculo de la condición climática del día dependiendo de la posición de los planetas.
+	 */
 	w.setWeatherCondition()
 }
 
+//Método encargado de asignar la condición climática del día.
 func (w Weather) setWeatherCondition() {
+	/**
+	 * Se obtienen las coordenadas cartesianas de cada planeta a partir de sus coordenadas polares.
+	 */
 	betasoideCartesianCoordinate := w.calculateCartesianCoordinateFromStar(w.Betasoide)
 	vulcanoCartesianCoordinate := w.calculateCartesianCoordinateFromStar(w.Vulcano)
 	ferengiCartesianCoordinate := w.calculateCartesianCoordinateFromStar(w.Ferengi)
 	sunCartesianCoordinate := w.calculateCartesianCoordinateFromStar(w.sun)
+
+	/**
+	 * Se realiza el llamado del método que devuelve la función que calcula la pendiente de la recta
+	 * formada a partir del planeta más lejano (Betasoide).
+	 */
+	functionToCalculateSlope := utils.GenerateFunctionToCalculateSlope(&betasoideCartesianCoordinate)
+
+	/**
+	 * Se calculan las pendientes de las 3 rectas formadas a partir del planeta más lejano (Betasoide).
+	 */
+	slopeBetasoideFerengi := functionToCalculateSlope(&ferengiCartesianCoordinate)
+	slopeBetasoideVulcano := functionToCalculateSlope(&vulcanoCartesianCoordinate)
+	slopeBetasoideSun := functionToCalculateSlope(&sunCartesianCoordinate)
+
+	/**
+	 * Se comparan las pendientes entre betasoide-ferengi y betasoide-vulcano.
+	 * Si son iguales, quiere decir que los 3 planetas están alineados.
+	 */
+	if slopeBetasoideFerengi == slopeBetasoideVulcano {
+		/**
+		 * Se compara la pendiente de cualquier recta formada entre los planetas con la pendiente
+		 * formada entre el planeta más lejano (Betasoide) y el Sol.
+		 * Si son iguales, quiere decir que los 3 planetas están alineados con el sol, lo cual
+		 * significa que habrá sequía.
+		 * Si son diferentes, quiere decir que los 3 planetas están alineados pero no con el sol, lo
+		 * cual significa que habrán condiciones óptimas de presión y temperatura.
+		 */
+		if slopeBetasoideFerengi == slopeBetasoideSun {
+			w.WeatherCondition = Dry
+		} else {
+			w.WeatherCondition = Optimal
+		}
+	} else {
+		/**
+		 * Al no estár alineados, los planetas forman un triángulo.
+		 * Para calcular el perímetro de cualquier triángulo, se requiere conocer la longitud de sus lados,
+		 * para así sumarlos y hallar el valor.
+		 */
+
+		/**
+		 * Se calculan las distancias entre los 3 planetas.
+		 */
+		distanceBetasoideFerengi := utils.GetDistanceBetweenPoints(&betasoideCartesianCoordinate, &ferengiCartesianCoordinate)
+		distanceBetasoideVulcano := utils.GetDistanceBetweenPoints(&betasoideCartesianCoordinate, &vulcanoCartesianCoordinate)
+		distanceVulcanoFerengi := utils.GetDistanceBetweenPoints(&vulcanoCartesianCoordinate, &ferengiCartesianCoordinate)
+
+		/**
+		 * Se calcula el perímetro del triángulo, mediante la suma de sus lados.
+		 * Posteriormente, se revisará en la clase {@link SolarSystem} el perímetro más alto,
+		 * lo cual servirá para detectar los días con picos altos de lluvia; éstos corresponden
+		 * a aquellos días que compartan ese valor del perímetro mayor.
+		 */
+		w.Perimeter = distanceBetasoideFerengi + distanceBetasoideVulcano + distanceVulcanoFerengi
+
+		/**
+		 * Se realiza el llamado al método encargado de revisar si el sol se encuentra dentro del triángulo
+		 * formado entre los 3 planetas.
+		 * Si lo está, quiere decir que la condición del clima será de lluvia.
+		 */
+		if utils.EvaluateIfPointIsInsideTheTriangle(&betasoideCartesianCoordinate, &ferengiCartesianCoordinate, &vulcanoCartesianCoordinate, &sunCartesianCoordinate) {
+			w.WeatherCondition = Rain
+		}
+	}
 }
 
+//Método para calcular la coordenada cartesiana a partir de la coordenada polar.
 func (w Weather) calculateCartesianCoordinateFromStar(star *Star) model.CartesianCoordinate {
 	/**
 	 * Llamado del método de cálculo presente en utils.go.
 	 */
-	return utils.GetCartesianCoordinateFromPolarCoordinate(&star.PolarCoordinate)
+	return utils.GetCartesianCoordinateFromPolarCoordinate(star.PolarCoordinate)
 }
--- functions/src/shareddomain/utils.go
+++ functions/src/shareddomain/utils.go
@@ -5,7 +5,7 @@ import (
 	"math"
 	"time"
 
-	model "github.com/cedv1990/weather-predictor-go/functions/src/model"
+	"github.com/cedv1990/weather-predictor-go/functions/src/model"
 )
 
 const (
@@ -36,4 +36,73 @@ func GetCartesianCoordinateFromPolarCoordinate(polar *model.PolarCoordinate) mod
 	y := float64(polar.Radius) * math.Sin(radiansAngle)
 
 	return model.CartesianCoordinate{X: x, Y: y}
+}
+
+//GenerateFunctionToCalculateSlope Método que devuelve la función que calcula la pendiente formada entre dos puntos, a partir de sus coordenadas cartesianas.
+func GenerateFunctionToCalculateSlope(from *model.CartesianCoordinate) func(to *model.CartesianCoordinate) float64 {
+	return func(to *model.CartesianCoordinate) float64 {
+		x1, y1 := from.X, from.Y
+		x2, y2 := to.X, to.Y
+
+		m := GetSlope(x1, y1, x2, y2)
+
+		return Round(m, 10)
+	}
+}
+
+//GetSlope Método que calcula la pendiente a partir de dos puntos cardenales.
+func GetSlope(x1, y1, x2, y2 float64) float64 {
+	return (y2 - y1) / (x2 - x1)
+}
+
+//Round Método para aproximar números y convertir un número a X cantidad de dígitos decimales.
+func Round(decimal float64, to int) float64 {
+	tof := float64(to)
+	return math.Round(decimal*tof) / tof
+}
+
+//GetDistanceBetweenPoints Método que calcula la distancia que hay entre 2 puntos cartesianos.
+func GetDistanceBetweenPoints(from, to *model.CartesianCoordinate) float64 {
+	co := math.Pow(to.X-from.X, 2)
+	ca := math.Pow(to.Y-from.Y, 2)
+
+	return math.Sqrt(ca + co)
+}
+
+//EvaluateIfPointIsInsideTheTriangle Método que evalúa si un punto P se encuentra dentro del perímetro de un triángulo formado por los puntos A + B + C.
+func EvaluateIfPointIsInsideTheTriangle(a, b, c, p *model.CartesianCoordinate) bool {
+	/**
+	 * Fórmula tomada de {@link "https://huse360.home.blog/2019/12/14/como-saber-si-un-punto-esta-dentro-de-un-triangulo/"}
+	 */
+
+	/**
+	 * Segmento del triángulo resultado de B - A
+	 */
+	d := model.CartesianCoordinate{X: b.X - a.X, Y: b.Y - a.Y}
+
+	/**
+	 * Segmento del triángulo resultado de C - A
+	 */
+	e := model.CartesianCoordinate{X: c.X - a.X, Y: c.Y - a.Y}
+
+	/**
+	 * Variable de ponderación a ~ b (Vector de "a" hacia "b". Segmento que sumado a w2 da la ubicación de P)
+	 * w1 = (Ex*(Ay + Py) + Ey*(Px - Ax)) / (Dx*Ey - Dy*Ex)
+	 */
+	w1 := (e.X*(a.Y-p.Y) + e.Y*(p.X-a.X)) / (d.X*e.Y - d.Y*e.X)
+
+	/**
+	 * Variable de ponderación a ~ c (Vector de "a" hacia "c". Segmento que sumado a w1 da la ubicación de P)
+	 * w2 = (1 / Ey) * (Py - Ay - w1*Dy)
+	 */
+	w2 := (p.Y - a.Y - w1*d.Y) / e.Y
+
+	/**
+	 * Si el vector w1 es positivo o igual a 0 y
+	 * si el vetor w2 es positivo o igual a 0 y
+	 * si la suma de ambos no excede 1.0,
+	 * quiere decir que el punto no está por fuera de los límites
+	 * del triángulo.
+	 */
+	return (w1 >= 0.0) && (w2 >= 0.0) && ((w1 + w2) <= 1.0)
 }
